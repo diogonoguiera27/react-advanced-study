d51b49027ae81307a03c1e8bbf1732bc66ca28f1
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-16 14:17:04 -0300

    Propriedade Key

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -3,13 +3,25 @@ import React, { useState } from 'react'
 const ListRender = () => {
 
     const [list] = useState(["Diogo","tayllan","Kaio"]);
+    const [ users] = useState([
+        {id:1, name: "Diogo" ,age:20},
+        {id:421342352, name: "Lucas" ,age:26},
+        {id:4523454365, name: "papa" ,age:50},
+    ]);
   return (
     <div>
         <ul>
-            {list.map((item)=>(
-                <li>
+            {list.map((item,i)=>(
+                <li key={i}>
                     {item}
                 </li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((user) =>(
+              <li key={user.id}>
+                {user.name} - {user.age}
+              </li>
             ))}
         </ul>
     </div>
