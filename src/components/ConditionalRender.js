9ca77dbeef7fceea12f34f4b0d480d74190a9cd4
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-16 14:53:26 -0300

    Renderização Condicional com if  e else

--- src/components/ConditionalRender.js
+++ src/components/ConditionalRender.js
@@ -3,10 +3,28 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
     const [ x ] = useState(true);
+
+    const [name,setName] = useState("Diogo")
   return (
     <div>
         <h1>isso Sera Exibido?</h1>
         {x && <p>se x for true , sim ?</p> }
+        <h1>if Iternario</h1>
+        {name === "Diogo" ? (
+            <div>
+                <p>
+                    o nome e joão
+                </p>
+            </div>
+        ):(
+            <div>
+                <p>
+                    nome nao encontrado!!
+                </p>
+            </div>
+            
+        )}
+        <button onClick={() =>setName("Diogo")}>click aqui </button>
     </div>
     
   )
