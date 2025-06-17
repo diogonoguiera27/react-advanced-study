d305187a0c0158da7d4ffae023a6fb0794336ddd
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-17 09:08:13 -0300

    Renderização de lista em Componentes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
 //Components
-// importação isso e a maneira de reutilizar o components que foram criados em arquivos separados  
+// importação isso e a maneira de reutilizar o components que foram criados em arquivos separados
 // beneficios conseguimos manter oa interface organizasdas
 // sintaxe :  import nome do arquivo ( deve esta com a Primeira letra Maiucsular ) from './components/FristComponents'--> (Caminho Relativo: ate o arquivo onmde esta salvo );
-import FristComponents from './components/FristComponents';
-import TemplateExpressions from './components/TemplateExpressions';
-import MyComponents from './components/MyComponents';
-import Events from './components/Events';
-import Challenge from './components/Challenge';
+import FristComponents from "./components/FristComponents";
+import TemplateExpressions from "./components/TemplateExpressions";
+import MyComponents from "./components/MyComponents";
+import Events from "./components/Events";
+import Challenge from "./components/Challenge";
 // Styles / css
-import './App.css';
-import ListRender from './components/ListRender';
-import ConditionalRender from './components/ConditionalRender';
-import ShowUserName from './components/ShowUserName';
-import { useState } from 'react';
-import CarDetails from './components/CarDetails';
-
-
-
-
+import "./App.css";
+import ListRender from "./components/ListRender";
+import ConditionalRender from "./components/ConditionalRender";
+import ShowUserName from "./components/ShowUserName";
+import { useState } from "react";
+import CarDetails from "./components/CarDetails";
 
 function App() {
-  const name = "Lucas"
-  const [userName] = useState('Maria')
+  const name = "Lucas";
+  const [userName] = useState("Maria");
+
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "vermelha", km: 0, newCar: true },
+    { id: 2, brand: "gol", color: "branco", km: 2500, newCar: false },
+    { id: 3, brand: "uno", color: "vinho", km: 1232, newCar: false },
+  ];
   return (
     <div className="App">
       <h1> Fundamentos React </h1>
-      <FristComponents/>
+      <FristComponents />
       <TemplateExpressions />
-      <MyComponents/>
-      <Events/>
-      <Challenge/>
-      <ListRender/>
-      <ConditionalRender/>
-      <ShowUserName name={userName}/>
-      <CarDetails brand="vw" km={10000} cor="azul" newCar={false}/>
-      <CarDetails brand="fiat" km={0} cor="branco" newCar={true}/>
-      <CarDetails brand="" km={10000} cor="azul"  newCar={false}/>
+      <MyComponents />
+      <Events />
+      <Challenge />
+      <ListRender />
+      <ConditionalRender />
+      <ShowUserName name={userName} />
+      <CarDetails brand="vw" km={10000} cor="azul" newCar={false} />
+      <CarDetails brand="fiat" km={0} cor="branco" newCar={true} />
+      <CarDetails brand="dasd" km={10000} cor="azul" newCar={false} />
+
+      {/* loop em  array  de objetos */}
+      {cars.map((car) => (
+        <CarDetails
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
     </div>
   );
 }
